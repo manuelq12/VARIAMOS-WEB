aaf593636af8bd2c84401618422b177a7bafa711
saradrada <saradrada@hotmail.com>
2019-06-07 09:54:50 -0500

    Submit frontend working. Compile microservice working

--- src/components/model_actions/Verification.vue
+++ src/components/model_actions/Verification.vue
@@ -24,117 +24,136 @@
 
 <script>
 import axios from "axios";
-import { setupModal, modalH3, modalSimpleText } from '../../assets/js/common/util'
+import {
+  setupModal,
+  modalH3,
+  modalSimpleText
+} from "../../assets/js/common/util";
 
 export default {
-  data: function(){
+  data: function() {
     return {
-      model_data:"", //stores the mxgraph model data
-      errors:[], //errors
-      modelType:"", //stores the model type (feature, component, etc)
+      model_data: "", //stores the mxgraph model data
+      errors: [], //errors
+      modelType: "", //stores the model type (feature, component, etc)
       menu_options: [], //stores the custom verification menu options
-      current_verification:"", //stores the current verification function
-      cell_errors:[], //stores the cells that contains errors
-      cell_overlays:[] //stores the overlays that show errors
-    }
+      current_verification: "", //stores the current verification function
+      cell_errors: [], //stores the cells that contains errors
+      cell_overlays: [] //stores the overlays that show errors
+    };
   },
   props: {
-   current_graph: {
-    type: Object,
-    required: true
-   }
+    current_graph: {
+      type: Object,
+      required: true
+    }
   },
-  mounted: function(){
+  mounted: function() {
     this.initialSetup();
   },
-  watch:{
-    $route (to, from){
-      if(this.$route.name === 'Models')
-      {
+  watch: {
+    $route(to, from) {
+      if (this.$route.name === "Models") {
         this.initialSetup();
       }
     }
   },
   methods: {
     // executes an initial setup in which the custom model verification file is loaded
-    initialSetup(){
-      this.modelType=this.$route.params.type;
-      this.menu_options=[];
+    initialSetup() {
+      this.modelType = this.$route.params.type;
+      this.menu_options = [];
       try {
         //load custom model verification file
-        var verificationToImport = require('@/assets/js/models/custom/verification/'+this.modelType+'.js');
+        var verificationToImport = require("@/assets/js/models/custom/verification/" +
+          this.modelType +
+          ".js");
         this.currentVerification = verificationToImport.default();
-        this.menu_options=this.currentVerification;
+        this.menu_options = this.currentVerification;
       } catch (ex) {
         //no verification available - nothing to do
       }
     },
     //clear all the overlays
-    clear_overlays(){
+    clear_overlays() {
       for (var i = 0; i < this.cell_errors.length; i++) {
-        this.current_graph.removeCellOverlay(this.cell_errors[i], this.cell_overlays[i]);
+        this.current_graph.removeCellOverlay(
+          this.cell_errors[i],
+          this.cell_overlays[i]
+        );
       }
-      this.cell_errors=[];
-      this.cell_overlays=[];
+      this.cell_errors = [];
+      this.cell_overlays = [];
     },
     //executes a test sending the current mxgraph model to the backend server
-         test_micro(){
-     if (localStorage["domain_implementation_main_path"]) {
-       this.errors=[];
-       var encoder = new mxCodec();
-       var result = encoder.encode(this.current_graph.getModel());
-       var xml = mxUtils.getXml(result);
-       axios.post(localStorage["domain_implementation_main_path"]+'NewMicro/test', {
-         data: "test"
-       })
-       .then(response => {
-         var c_header = modalH3("Test response");
-         var c_body = modalSimpleText(response.data);
-         setupModal(c_header,c_body);
-       })
-       .catch(e => {
-         this.errors.push(e);
-         var c_header = modalH3(this.$t("modal_error"),"error");
-         var c_body = modalSimpleText(e + this.$t("model_actions_backend_problem"));
-         setupModal(c_header,c_body);
-       });
-     }else{
-       var c_header = modalH3(this.$t("modal_error"),"error");
-       var c_body = modalSimpleText(this.$t("verification_path_problem"));
-       setupModal(c_header,c_body);
-     }
-    
-   },
-
-    test(){
+    test_micro() {
       if (localStorage["domain_implementation_main_path"]) {
-        this.errors=[];
+        this.errors = [];
         var encoder = new mxCodec();
         var result = encoder.encode(this.current_graph.getModel());
         var xml = mxUtils.getXml(result);
-        axios.post(localStorage["domain_implementation_main_path"]+'Verification/test', {
-          data: xml
-        })
-        .then(response => {
-          var c_header = modalH3("Test response");
-          var c_body = modalSimpleText(response.data);
-          setupModal(c_header,c_body);
-        })
-        .catch(e => {
-          this.errors.push(e); 
-          var c_header = modalH3(this.$t("modal_error"),"error");
-          var c_body = modalSimpleText(e + this.$t("model_actions_backend_problem"));
-          setupModal(c_header,c_body);
-        });
-      }else{
-        var c_header = modalH3(this.$t("modal_error"),"error");
+        axios
+          .post(
+            localStorage["domain_implementation_main_path"] + "NewMicro/test",
+            {
+              data: "test"
+            }
+          )
+          .then(response => {
+            var c_header = modalH3("Test response");
+            var c_body = modalSimpleText(response.data);
+            setupModal(c_header, c_body);
+          })
+          .catch(e => {
+            this.errors.push(e);
+            var c_header = modalH3(this.$t("modal_error"), "error");
+            var c_body = modalSimpleText(
+              e + this.$t("model_actions_backend_problem")
+            );
+            setupModal(c_header, c_body);
+          });
+      } else {
+        var c_header = modalH3(this.$t("modal_error"), "error");
         var c_body = modalSimpleText(this.$t("verification_path_problem"));
-        setupModal(c_header,c_body);
+        setupModal(c_header, c_body);
       }
-      
+    },
+
+    test() {
+      if (localStorage["domain_implementation_main_path"]) {
+        this.errors = [];
+        var encoder = new mxCodec();
+        var result = encoder.encode(this.current_graph.getModel());
+        var xml = mxUtils.getXml(result);
+        axios
+          .post(
+            localStorage["domain_implementation_main_path"] +
+              "Verification/test",
+            {
+              data: xml
+            }
+          )
+          .then(response => {
+            var c_header = modalH3("Test response");
+            var c_body = modalSimpleText(response.data);
+            setupModal(c_header, c_body);
+          })
+          .catch(e => {
+            this.errors.push(e);
+            var c_header = modalH3(this.$t("modal_error"), "error");
+            var c_body = modalSimpleText(
+              e + this.$t("model_actions_backend_problem")
+            );
+            setupModal(c_header, c_body);
+          });
+      } else {
+        var c_header = modalH3(this.$t("modal_error"), "error");
+        var c_body = modalSimpleText(this.$t("verification_path_problem"));
+        setupModal(c_header, c_body);
+      }
     }
   }
-}
+};
 </script>
 
 <style scoped>
--- src/views/Coffee.vue
+++ src/views/Coffee.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-container class="bv-example-row">
+    <b-container>
       <b-row>
         <b-col cols="1">
           <h1 class="h2">{{ $t("app_menu_coffee") }}</h1>
@@ -15,26 +15,53 @@
       <b-tabs content-class="mt-3">
         <b-tab title="HLVL" active>
           <div class="div-text-area">
-            <b-container class="bv-example-row">
+            <b-container>
               <div>
+                <div>
+                  <br>
+                  <!-- Selected file: {{file && file.name}}-->
+                  <b-form-file
+                    v-model="file"
+                    ref="file"
+                    id="file"
+                    class="mb-2"
+                    accept=".hlvl, .sfxm, .xml, .tvl, .txt"
+                    placeholder="Choose a model..."
+                    v-on:change="handleFileUpload()"
+                  ></b-form-file>
+
+                  <b-button v-on:click="test_submit" class="mr-2">
+                    <i class="fas fa-upload fa-sm text-white-50"></i>
+                    Submit
+                  </b-button>
+                  <b-button class="btn btn-danger" @click="file = null">Cancel</b-button>
+                </div>
+                <br>
+
                 <b-row>
                   <b-col cols="8">
-                    <label>HLVL Editor</label>
+                    <h6 class="m-0 font-weight-bold">HLVL Editor</h6>
                   </b-col>
 
                   <b-col cols="4">
-                    <label><strong>Model Configuration</strong></label>
+                    <h6 class="m-0 font-weight-bold">Model Configuration</h6>
                   </b-col>
                 </b-row>
               </div>
               <b-row>
                 <b-col cols="8">
-                  <b-form-textarea id="textarea" v-model="input" rows="4" max-rows="10"></b-form-textarea>
+                  <b-form-textarea
+                    id="textarea-hlvl-editor"
+                    v-model="input_hlvl_editor"
+                    rows="4"
+                    max-rows="10"
+                    placeholder="HLVL Editor"
+                  ></b-form-textarea>
                 </b-col>
 
                 <b-col cols="4">
                   <b-form-textarea
-                    id="textarea-configuracion"
+                    id="textarea-configuration"
                     v-model="configuration"
                     rows="4"
                     max-rows="10"
@@ -43,7 +70,7 @@
               </b-row>
 
               <br>
-              <b-button v-on:click="compile">Compile code</b-button>
+              <b-button v-on:click="test_micro">Compile code</b-button>
               <br>
               <br>
             </b-container>
@@ -52,41 +79,46 @@
               <div>
                 <b-row>
                   <b-col cols="8">
-                    <label>HLVL Console</label>
+                    <h6 class="m-0 font-weight-bold">HLVL Console</h6>
                   </b-col>
 
                   <b-col cols="4">
-                    <label><strong>Statistics</strong></label>
+                    <h6 class="m-0 font-weight-bold">Statistics</h6>
                   </b-col>
                 </b-row>
               </div>
               <b-row>
                 <b-col cols="8">
-                  <b-form-textarea id="textarea" v-model="response" rows="4" max-rows="10"></b-form-textarea>
+                  <b-form-textarea
+                    id="textarea-hlvl-console"
+                    v-model="input_hlvl_console"
+                    rows="4"
+                    max-rows="10"
+                  ></b-form-textarea>
                 </b-col>
                 <b-col cols="4">
                   <table class="table table-hover">
                     <tbody>
                       <tr>
                         <td style="width: 70%">Satisfactory model:</td>
-                        <td style="width: 30%">YES</td>
+                        <td style="width: 30%" id="satisfactory_model"></td>
                       </tr>
                       <tr>
                         <td>Number of products that can be generated:</td>
-                        <td>100</td>
+                        <td></td>
                       </tr>
                       <tr>
                         <td>Number of elements:</td>
-                        <td>100</td>
+                        <td></td>
                       </tr>
                       <tr>
                         <td>Number of relationships (by type):</td>
-                        <td>100</td>
+                        <td></td>
                       </tr>
 
                       <tr>
                         <td>Overall time of analysis:</td>
-                        <td>30 sec</td>
+                        <td></td>
                       </tr>
                     </tbody>
                   </table>
@@ -99,11 +131,11 @@
         <b-tab title="HLCL">
           <div class="div-text-area">
             <div>
-              <label>HLCL</label>
+              <h6 class="m-0 font-weight-bold">HLCL</h6>
             </div>
 
             <div>
-              <b-form-textarea id="textarea" v-model="input" rows="10"></b-form-textarea>
+              <b-form-textarea id="textarea-hlcl" v-model="input_hlcl" rows="10"></b-form-textarea>
             </div>
           </div>
         </b-tab>
@@ -111,11 +143,11 @@
         <b-tab title="Solving">
           <div class="div-text-area">
             <div>
-              <label>Solving</label>
+              <h6 class="m-0 font-weight-bold">Solving</h6>
             </div>
 
             <div>
-              <b-form-textarea id="textarea" v-model="input" rows="10"></b-form-textarea>
+              <b-form-textarea id="textarea-solving" v-model="input_solving" rows="10"></b-form-textarea>
             </div>
           </div>
         </b-tab>
@@ -123,11 +155,11 @@
         <b-tab title="Solution">
           <div class="div-text-area">
             <div>
-              <label>Solution</label>
+              <h6 class="m-0 font-weight-bold">Solution</h6>
             </div>
 
             <div>
-              <b-form-textarea id="textarea" v-model="input" rows="10"></b-form-textarea>
+              <b-form-textarea id="textarea-solution" v-model="input_solution" rows="10"></b-form-textarea>
             </div>
           </div>
         </b-tab>
@@ -143,30 +175,61 @@
 </template>
 
 <script>
+import axios from "axios";
+import { setupModal, modalH3, modalSimpleText } from "../assets/js/common/util";
 export default {
   name: "coffee",
   data() {
     return {
-      input: "",
-      response: [],
-      errors: []
+      input_hlvl_editor: "",
+      errors: [],
+      file: null
     };
   },
   methods: {
-    compile: function() {
-      alert("Compile");
-      AXIOS.get(`http://localhost:8888/variamosbackend/coffee`, {
-        params: {
-          input: this.input
-        }
-      }) // base URL is in http-commons.js
-        .then(response => {
-          // JSON responses are automatically parsed.
-          this.response = response.data["content"];
-        })
-        .catch(e => {
-          this.errors.push(e);
-        });
+    function1() {
+      alert(this.input_hlvl_editor);
+    },
+
+    test_micro() {
+      alert(this.input_hlvl_editor);
+      if (localStorage["domain_implementation_main_path"]) {
+        this.errors = [];
+        axios
+          .post(
+            localStorage["domain_implementation_main_path"] + "NewMicro/test",
+            {
+              data: this.input_hlvl_editor
+            }
+          )
+          .then(response => {
+            var c_header = modalH3("Test response");
+            var c_body = modalSimpleText(response.data);
+            var console = document.getElementById("textarea-hlvl-console");
+            console.value = response.data;
+            setupModal(c_header, c_body);
+          })
+          .catch(e => {
+            this.errors.push(e);
+            var c_header = modalH3(this.$t("modal_error"), "error");
+            var c_body = modalSimpleText(
+              e + this.$t("model_actions_backend_problem")
+            );
+            setupModal(c_header, c_body);
+          });
+      } else {
+        var c_header = modalH3(this.$t("modal_error"), "error");
+        var c_body = modalSimpleText(this.$t("verification_path_problem"));
+        setupModal(c_header, c_body);
+      }
+    },
+    clearFiles() {
+      this.$refs["file-input"].reset();
+    },
+    handleFileUpload() {},
+
+    test_submit() {
+      alert("Submit: " + this.file.name);
     }
   }
 };
@@ -174,4 +237,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.font-weight-bold {
+  padding: 5px;
+}
 </style>
